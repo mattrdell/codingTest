08d1f9bcd4cbb173f37442e8b6ce4a571509038e
Matthew Dell <mattrdell@gmail.com>
2016-01-17 18:21:23 -0500

    Installed Moq and worked on unit tests (not exactly TDD but will be good for testing)

--- CodingTest.DAL/Entities/Item.cs
+++ CodingTest.DAL/Entities/Item.cs
@@ -8,9 +8,6 @@ namespace CodingTest.DAL.Entities
         [Key]
         public int ItemId { get; set; }
         public int QtyInStock { get; set; }
-        [NotMapped]
-        public bool IsPurchase { get; set; }
-
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
--- CodingTest.WebAPI.Tests/Repositories/ItemRepositoryTest.cs
+++ CodingTest.WebAPI.Tests/Repositories/ItemRepositoryTest.cs
@@ -1,14 +1,58 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using CodingTest.DAL.DataContexts;
+using CodingTest.DAL.Entities;
+using CodingTest.DAL.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CodingTest.WebAPI.Tests.Repositories
 {
     [TestClass]
     public class ItemRepositoryTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Mock<ItemDbCtx> mockContext;
+
+        [TestInitialize]
+        public void Setup()
         {
+            var data = new List<Item>
+            {
+                new Item {Name = "Item1", Description = "Item Num 1", ItemId = 0, Price = 5, QtyInStock = 10},
+                new Item {Name = "Item2", Description = "Item Num 2", ItemId = 1, Price = 15, QtyInStock = 0},
+                new Item {Name = "Item3", Description = "Item Num 3", ItemId = 2, Price = 25, QtyInStock = 30}
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Item>>();
+            mockSet.As<IQueryable<Item>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Item>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Item>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Item>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            mockContext = new Mock<ItemDbCtx>();
+            mockContext.Setup(m => m.Items).Returns(mockSet.Object);
+        }
+
+
+        [TestMethod]
+        public void TestPurchaseItem()
+        {
+            var itemRepo = new ItemRepository(mockContext.Object);
+
+            var firstOrDefault = itemRepo.GetItems().FirstOrDefault(x => x.ItemId == 0);
+            Assert.IsTrue(firstOrDefault != null && firstOrDefault.QtyInStock == 10);
+
+            itemRepo.PurchaseItem(0);
+
+            firstOrDefault = itemRepo.GetItems().FirstOrDefault(x => x.ItemId == 0);
+            Assert.IsTrue(firstOrDefault != null && firstOrDefault.QtyInStock == 9);
+        }
+
+        [TestMethod]
+        public void TestGetItems()
+        {
+            var itemRepo = new ItemRepository(mockContext.Object);
         }
     }
 }
